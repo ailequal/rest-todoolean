601b1983c8c824d7e08aab8c2afc6123ce62a23e
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-19 18:09:58 +0100

    Added DELETE

--- js/script.js
+++ js/script.js
@@ -10,10 +10,25 @@ $(document).ready(function () {
 			console.log('newTodo', newToDo);
 			$(this).val('');
 			addToDo(newToDo);
+			// refresh the to-do
 			setTimeout(printToDo, 500);
 		}
 	});
 
+	// delete the selected to-do
+	$(document).on('click', '.cross', function () {
+		var id = $(this).closest('li').attr('id-attribute');
+		console.log(id);
+		deleteToDo(id);
+		// refresh the to-do
+		setTimeout(printToDo, 500);
+	});
+
+	// toggle checked item
+	$(document).on('click', '.check', function () {
+		console.log($(this).closest('li'));
+		$(this).closest('li').toggleClass('line-through');
+	});
 
 	// functions
 	// print all the to-do available on the server
@@ -27,11 +42,13 @@ $(document).ready(function () {
 				console.log('printTodo()', data);
 				// scan every to-do and append it to the html
 				for (var i = 0; i < data.length; i++) {
+					var id = data[i].id;
 					var toDo = data[i].text;
 					// handlebars
 					var source = $('#template').html();
 					var template = Handlebars.compile(source);
 					var context = {
+						idAttribute: id,
 						toDo: toDo
 					};
 					var html = template(context);
@@ -54,6 +71,37 @@ $(document).ready(function () {
 			},
 			success: function (data, state) {
 				console.log('addToDo()', data);
+			},
+			error: function (request, state, error) {
+				console.log(error);
+			}
+		});
+	}
+
+	// add a new to-do to the server
+	function updateToDo(updatedToDo) {
+		$.ajax({
+			url: "http://157.230.17.132:3027/todos/" + x,
+			method: "PATCH",
+			data: {
+				text: updatedToDo,
+			},
+			success: function (data, state) {
+				console.log('updateToDo()', data);
+			},
+			error: function (request, state, error) {
+				console.log(error);
+			}
+		});
+	}
+
+	// delete a to-do from the server
+	function deleteToDo(id) {
+		$.ajax({
+			url: "http://157.230.17.132:3027/todos/" + id,
+			method: "DELETE",
+			success: function (data, state) {
+				console.log('deleteToDo()', data);
 			},
 			error: function (request, state, error) {
 				console.log(error);
